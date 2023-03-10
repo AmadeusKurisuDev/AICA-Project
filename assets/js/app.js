0a9474331a801379317a7761d33e45a8025b2fc8
Michele Felici <michelefelice58@gmail.com>
2023-03-10 11:36:04 +0100

    edited copyright on leaflet on app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([42.30031812670491, 12.412028984726453], 50);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.aicanet.it/">AICA</a>'
+    attribution: '&copy; <a href="https://8digital.it/">8Digital</a>'
 }).addTo(map);
 
 L.marker([42.30031812670491, 12.412028984726453]).addTo(map)
